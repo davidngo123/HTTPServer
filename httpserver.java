8cfae5d66237f54774e5aab1dfd7f18fa3262932
davidngo123 <ngodavid215@gmail.com>
2023-04-25 23:52:14 -0700

    finished get method

--- httpserver.java
+++ httpserver.java
@@ -1,95 +1,108 @@
 import java.net.*;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
 import java.nio.file.Paths;
 
+
 public class httpserver {
-    private static String response;
-    private static int port;
+
+    static int port = 80;
+    static OutputStream os;
+    static Socket clientSocket;
     private static Map<String, String> mimeType;
-    public static void main(String[] args) {
-        port = 80;
-        // A reference of the client socket
-        Socket socket;
-        //file type conversion
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Server running on port: " + port);
+
         mimeType = new HashMap<String, String>();
         mimeType.put("txt", "text/plain");
         mimeType.put("html", "text/html");
 
 
-        try {
-            // Setup the server socket
-            ServerSocket serverSocket = new ServerSocket(port);
-            System.out.println("Web Server is starting up, listening at port " + port);
+        while (true) {
+            try {
+                clientSocket = serverSocket.accept();
+                System.out.println("Client Connected");
 
-            while (true) {
-                // Make the server socket wait for the next client request
-                socket = serverSocket.accept();
-                // Local reader from the client
-                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                os = clientSocket.getOutputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
                 // Assign http request
                 String clientRequest = reader.readLine();
-                System.out.println(clientRequest);
-                if (clientRequest.startsWith("GET")) {
-                    response = handleGetRequest(clientRequest);
-                    System.out.println(response);
-//                } else if (clientRequest.startsWith("POST")) {
-//                    response = handlePostRequest(clientRequest);
-//                } else if(clientRequest.startsWith("PUT")){
-//                    response = handlePutRequest(clientRequest);
-//                } else if(clientRequest.startsWith("DELETE")){
-//                    response = handleDeleteRequest(clientRequest);
-//                } else {
-//
-//                }
-//
-//                OutputStream outputStream = clientSocket.getOutputStream();
-//                outputStream.write(response.getBytes());
-//                outputStream.flush();
-//
-//                System.out.println("Client Disconnected");
-//                clientSocket.close();
-//                reader.close();
+                String[] splitBySpace = clientRequest.split(" ");
+                System.out.println(splitBySpace[1]);
+                String[] splitByPeriod = splitBySpace[1].split("\\.");
+                String mime = mimeType.get(splitByPeriod[0]);
+                Path fileName = Paths.get("./" + splitBySpace[1]);
+                System.out.println(fileName);
+                if(clientRequest.startsWith("GET")) {
+                    handleGetRequest(splitBySpace[1], mime);
                 }
+            } catch(IOException ex) {
+                ex.printStackTrace();
             }
-        } catch (IOException ex) {
-            //Handle the exception
-            System.out.println(ex);
+
         }
     }
 
-    public static String handleGetRequest(String request){
-        String[] splitBySpace = request.split(" ");
-        System.out.println(splitBySpace[1]);
-        String[] splitByPeriod = splitBySpace[1].split("\\.");
-        String mime = mimeType.get(splitByPeriod[0]);
-        Path fileName = Paths.get("./" + splitBySpace[1]);
+    public static void handleGetRequest(String filename, String type){
+        Path fileName = Paths.get("./" + filename);
         System.out.println(fileName);
-        String strResponse = "";
 
         try {
             if(!Files.exists(fileName)){
-                byte[] bytes = Files.readAllBytes(Paths.get("cat404.jfif"));
-                String base64 = java.util.Base64.getEncoder().encodeToString(bytes);
-                String html = "<html><body><h1>Error 404</h1>" +
-                        "<p>File Found</p>" +
-                        "<img src='data:image/jpeg;base64,"
-                        + base64 + "'></body></html>";
-                return  "HTTP/1.1 404 Not Found \nFile not found\n\n" + html;
+                sendResponse(clientSocket,
+                        "404 NOT FOUND",
+                        0,
+                        mimeType.get(type),
+                        "".getBytes()
+                );
             } else {
-                strResponse = Files.readString(fileName);
+                String fileContent = Files.readString(fileName);
+                String content = fileContent;
+
+                sendResponse(clientSocket,
+                        "200 OK",
+                        ((fileContent).length()),
+                        mimeType.get(type),
+                        content.getBytes()
+                );
+
+
+
+
             }
         } catch (IOException ex) {
             //Handle the exception
             System.out.println(ex);
         }
-
-
-        return strResponse;
     }
 
 
+    private static void sendResponse(Socket client,
+                                     String status,
+                                     int contentLength,
+                                     String contentType,
+                                     byte[] content) throws IOException {
+
+
+        OutputStream output = client.getOutputStream();
+        os.write(("HTTP/1.1 "+ status + "\r\n").getBytes());
+        os.write(("Content-type: " + contentType).getBytes());
+        os.write("\r\n".getBytes());
+        os.write(("Content-length: " + contentLength).getBytes());
+        os.write("\r\n\r\n".getBytes());
+        os.write(content);
+        os.flush();
+        os.close();
+        client.close();
+    }
+
 }
+
+
